5567f0bb04c0104109662b0dac0e43ae7e2d9f8f
Bill Branan <bbranan@duraspace.org>
2015-06-22 11:53:29 -0400

    Initial commit of duracloud-db project. All the code included here is being transferred from the DuraCloud project: https://github.com/duracloud/duracloud. Source history prior to June 19, 2015 for these files can be found there.

---
+++ common/src/main/java/org/duracloud/common/db/error/NotFoundException.java
@@ -0,0 +1,21 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
+package org.duracloud.common.db.error;
+
+/**
+ * Exception thrown when a requested db value does not exist
+ *
+ * @author Bill Branan
+ */
+public class NotFoundException extends Exception {
+
+    public NotFoundException (String message) {
+        super(message);
+    }
+
+}
